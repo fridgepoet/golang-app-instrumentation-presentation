7014df645b713aaf94eb314f2bc63f007da05175
Shirley Leu <4163034+fridgepoet@users.noreply.github.com>
2025-01-29 17:28:07 +0100

    Remove some extra comments

--- otel.go
+++ otel.go
@@ -65,11 +65,8 @@ func setupOTelSDK(ctx context.Context) (shutdown func(context.Context) error, er
 	return
 }
 
-// Initialize a gRPC connection to be used by both the tracer and meter
-// providers.
+// Initialize a gRPC connection to be used by all providers
 func initConn() (*grpc.ClientConn, error) {
-	// It connects the OpenTelemetry Collector through local gRPC connection.
-	// You may replace `localhost:4317` with your endpoint.
 	conn, err := grpc.NewClient("otel-collector:4317",
 		// Note the use of insecure transport here. TLS is recommended in production.
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
